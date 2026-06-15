9abca1644dacde7ac6a5a3fd85a74824b9b6c4e9
stormy <artemolibozenko@gmail.com>
2026-06-15 15:42:46 +0300

    v1

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -21,8 +21,10 @@ class Category(models.Model):
     title = models.CharField(max_length=256, verbose_name='Заголовок')
     description = models.TextField(verbose_name='Описание')
     slug = models.SlugField(unique=True, verbose_name='Идентификатор',
-                            help_text='Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.')
-    is_published = models.BooleanField(default=True, verbose_name='Опубликовано', help_text='Снимите галочку, чтобы скрыть публикацию.')
+                            help_text='Идентификатор страницы для URL; '
+                                      'разрешены символы латиницы, цифры, дефис и подчёркивание.')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано',
+                                       help_text='Снимите галочку, чтобы скрыть публикацию.')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Добавлено')
 
     class Meta:
@@ -37,7 +39,8 @@ class Post(models.Model):
     title = models.CharField(max_length=256, verbose_name='Заголовок')
     text = models.TextField(verbose_name='Текст')
     pub_date = models.DateTimeField(verbose_name='Дата и время публикации',
-                                    help_text='Если установить дату и время в будущем — можно делать отложенные публикации.')
+                                    help_text='Если установить дату и время в будущем — '
+                                              'можно делать отложенные публикации.')
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                verbose_name='Автор публикации')
     location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True,
